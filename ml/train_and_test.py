6fc91948084eb152e5a750c6fba253618cffc563
Dylan Huang <dylan.p.huang@gmail.com>
2018-01-23 13:33:54 -0600

    Increased learning rate from 0.05 to 0.15

--- ml/train_and_test.py
+++ ml/train_and_test.py
@@ -27,7 +27,7 @@ params = {
     'objective': 'regression',
     'metric': {'l2', 'auc'},
     'num_leaves': 31,
-    'learning_rate': 0.05,
+    'learning_rate': 0.15,
     'feature_fraction': 0.9,
     'bagging_fraction': 0.8,
     'bagging_freq': 5,
