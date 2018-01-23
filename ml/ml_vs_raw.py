38754a0b7b5f6c7ee0d2f4459b9d9fc06cb60f35
Dylan Huang <dylan.p.huang@gmail.com>
2018-01-22 21:46:44 -0600

    Added file that compares ml model to traditional approach

---
+++ ml/ml_vs_raw.py
@@ -0,0 +1,35 @@
+import lightgbm as lgb
+import numpy as np
+import json
+import pdb
+
+ml_correct_count = 0.0
+raw_correct_count = 0.0
+num_questions = 0.0
+
+bst = lgb.Booster(model_file='./model.txt')
+
+questions_processed = json.load(open('questions_processed.json'))
+for question, data in questions_processed.iteritems():
+    num_questions += 1
+    maximum_value = -1
+    raw_answer = ""
+    for answer, value in data['raw_data']['fraction_answers'].iteritems():
+        if value > maximum_value:
+            maximum_value = value
+            raw_answer = answer
+
+    X_input = np.array(data['raw_data']['lines'])
+    Y_pred = bst.predict(X_input)
+    ml_answer = np.array(data['raw_data']['answers'])[np.where(Y_pred==Y_pred.max())][0]
+
+    for i in range(len(data['lines'])):
+        if data['lines'][i][0]:
+            correct_answer = data['raw_data']['answers'][i]
+
+    ml_correct_count += 1 if correct_answer == ml_answer else 0
+    raw_correct_count += 1 if correct_answer == raw_answer else 0
+
+print 'ML model accuracy: {}%'.format((ml_correct_count / num_questions) * 100)
+print 'Equally Weighting each approach accuracy: {}%'.format((raw_correct_count / num_questions) * 100)
+
